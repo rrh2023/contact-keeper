30312ab39410487ebed892a4a6124cd802e72046
Roberto Rodas-Herndon <rrh2023@bu.edu>
2025-09-20 11:39:23 -0400

    starting frontend

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = ({ title, icon }) => {
+  return <div></div>;
+};
+
+export default Navbar;
--- routes/contacts.js
+++ routes/contacts.js
@@ -57,15 +57,60 @@ router.post(
 // @route  PUT api/contacts/:id
 // @desc   update contact
 // @access Private
-router.put("/:id", (req, res) => {
-  res.send("Update contact");
+router.put("/:id", async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Build contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.name = phone;
+  if (type) contactFields.name = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true },
+    );
+
+    res.json(contact);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route  DELETE api/contacts/:id
 // @desc   Delete contact
 // @access Private
-router.delete("/:id", (req, res) => {
-  res.send("Delete contact");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: "Contact removed" });
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
